db8e7c24feabf3ce0e16e4b46aa36a35805ae99c
carlosismaelad <carlosismaelad@outlook.com>
2023-11-02 09:51:51 -0300

    Exercício antes do uso do Generics

---
+++ IntroGenerics/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace IntroGenerics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            PrintService pService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                pService.AddValue(x);
+            }
+            
+            pService.Print();
+            Console.WriteLine("First: " + pService.Firts());
+            
+        }
+    }
+}
