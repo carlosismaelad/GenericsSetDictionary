fbbddbc6cc1ad0bcef92004481ab08f60838bb7b
carlosismaelad <carlosismaelad@outlook.com>
2023-11-08 21:54:56 -0300

    Entendendo as comparações de GetHashCode, Equals e referência de memória.

---
+++ ComparandoUsuarios/Program.cs
@@ -0,0 +1,22 @@
+﻿using ComparandoUsuarios.Entities;
+
+namespace ComparandoUsuarios
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Client a = new Client { Name = "Maria Isabel", Email = "maria@mail.com" };
+            Client b = new Client { Name = "Ana Maria", Email = "maria@mail.com" };
+
+            Console.WriteLine(a.Equals(b)); // True porque usamos o "Email"
+            Console.WriteLine(a == b); //compara a referência do ponteiro de memória do objeto
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+
+        }
+    }
+}
