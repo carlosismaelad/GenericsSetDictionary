cb045f0b6e61683e31e43c4252e273287c164763
carlosismaelad <carlosismaelad@outlook.com>
2023-11-08 21:39:31 -0300

    Introdução sobre Equals e GetHashCode.

---
+++ EqualsAndGetHashCode/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EqualsAndGetHashCode
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            string a = "Maria";
+            string b = "Jose";
+            string c = "Maria";
+
+            Console.WriteLine("a é igual a b? " + a.Equals(b));
+            Console.WriteLine("a é igual a c? " + a.Equals(c));
+
+            Console.WriteLine();
+            Console.WriteLine("GetHashCode");
+            Console.WriteLine("HashCode de a: " + a.GetHashCode());
+            Console.WriteLine("HashCode de b: " + b.GetHashCode());
+            Console.WriteLine("HashCode de c: " + c.GetHashCode());
+
+        }
+    }
+
+}
