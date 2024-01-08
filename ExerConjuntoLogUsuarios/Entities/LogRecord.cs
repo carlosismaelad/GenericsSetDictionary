b44dac9c952e817e5460407b36020cb6e2481c4a
carlosismaelad <carlosismaelad@outlook.com>
2024-01-08 00:59:10 -0300

    Lendo 'arquivo de log', armazenando em HashSet e mostrando o total de usuários

---
+++ ExerConjuntoLogUsuarios/Entities/LogRecord.cs
@@ -0,0 +1,23 @@
+namespace ExerConjuntoLogUsuarios.Entities
+{
+    public class LogRecord
+    {
+        public string UserName { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+            return UserName.Equals(other.UserName);
+        }
+    }
+}
---
+++ ExerConjuntoLogUsuarios/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.Intrinsics.Arm;
+using ExerConjuntoLogUsuarios.Entities;
+
+namespace ExerConjuntoLogUsuarios
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+
+            Console.WriteLine("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(" ");
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { UserName = name, Instant = instant });
+                    }
+                    Console.WriteLine("Total user: " + set.Count);
+                }
+            }
+            catch (IOException error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
