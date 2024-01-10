0b470b0825d15e5b2b951e0d94099a588985d881
carlosismaelad <carlosismaelad@outlook.com>
2024-01-09 23:47:38 -0300

    Contagem de votos a partir de arquivo usando Dictionary

---
+++ ExerPropostoDictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ExerPropostoDictionary
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> votesForCandidates = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] votingRecord = sr.ReadLine().Split(",");
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if (votesForCandidates.ContainsKey(candidate))
+                        {
+                            votesForCandidates[candidate] += votes;
+                        }
+                        else
+                        {
+                            votesForCandidates[candidate] = votes;
+                        }
+                    }
+
+                    foreach (var item in votesForCandidates)
+                    {
+                        Console.WriteLine(item.Key + " : " + item.Value);
+                    }
+                }
+            }
+            catch (IOException error)
+            {
+                Console.WriteLine("An erro ocurred: " + error.Message);
+            }
+        }
+    }
+}
