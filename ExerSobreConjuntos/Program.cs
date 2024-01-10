6ab78dd3369991fbfc2de1021917f0cc458cc1b1
carlosismaelad <carlosismaelad@outlook.com>
2024-01-09 23:05:59 -0300

    Exer - contando elementos únicos em conjuntosdiferentes

---
+++ ExerSobreConjuntos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Xml.Schema;
+
+namespace ExerSobreConjuntos
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            HashSet<int> courseA = new HashSet<int>();
+            HashSet<int> courseB = new HashSet<int>();
+            HashSet<int> courseC = new HashSet<int>();
+
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int cod = int.Parse(Console.ReadLine());
+                courseA.Add(cod);
+            }
+
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int cod = int.Parse(Console.ReadLine());
+                courseB.Add(cod);
+            }
+
+            Console.Write("How many students for course C? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int cod = int.Parse(Console.ReadLine());
+                courseC.Add(cod);
+            }
+
+            HashSet<int> all = new HashSet<int>(courseA);
+            all.UnionWith(courseB);
+            all.UnionWith(courseC);
+
+            Console.WriteLine("Total students: " + all.Count);
+        }
+    }
+}
